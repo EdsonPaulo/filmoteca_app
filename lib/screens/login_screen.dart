d1adbd88616b74deb466d1afed7169990709aab2
Eliana Neto <eliananeto29@gmail.com>
2023-04-28 21:56:47 +0100

    refactoring

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/app_colors.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -8,14 +10,36 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  
+  final String image = 'assets/images/logo_b.png';
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color(0xFF181A1F),
+        iconTheme: IconThemeData(color: AppColors.primaryColor),
+      ),
+      backgroundColor: AppColors.primaryDarkColor,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(image),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.6),
+                BlendMode.srcATop,
+              ),
+            ),
+          ),
+        ),
+            
             // Adicione seus campos de e-mail e senha aqui
             const TextField(
               decoration: InputDecoration(labelText: 'Email'),
