7402a47b0b98cbc4d74f74da04a486facb456155
EdsonPaulo <edsonpaulo24@gmail.com>
2023-04-30 00:09:42 +0100

    refactor: profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:filmoteca_app/widgets/shared/custom_appbar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:filmoteca_app/utils/app_colors.dart';
 
@@ -16,14 +17,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
       backgroundColor: AppColors.darkPrimaryColor,
       appBar: const CustomHomeAppBar(),
       body: Center(
-        //padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            /*Text(
-                'PERFIL',
-                style: TextStyle(color: AppColors.primaryColor),
-              ),*/
             const SizedBox(height: 40),
             const CircleAvatar(
               radius: 40,
@@ -51,146 +47,158 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   color: Colors.white,
                   fontWeight: FontWeight.w400),
             ),
-            const SizedBox(height: 48),
-            Container(
-              width: 390,
-              height: 381,
-              decoration: const BoxDecoration(
-                color: Color(0xFF23252D),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
+            const SizedBox(height: 40),
+            Expanded(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                decoration: const BoxDecoration(
+                  color: AppColors.darkSecondaryColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25),
+                  ),
                 ),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Adicione aqui os widgets que você deseja exibir dentro da Column
-                  SizedBox(height: 10),
-
-                  TextButton(
-                    onPressed: () {
-                      // Adicione aqui o código que deve ser executado quando o botão for pressionado
-                    },
-                    child: Row(
-                      children: const [
-                        Icon(Icons.language_outlined, color: Colors.white),
-                        SizedBox(width: 8),
-                        Text(
-                          "Linguagem",
-                          style: TextStyle(
-                              fontSize: 16,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: const [
+                            Icon(CupertinoIcons.globe, color: Colors.white),
+                            SizedBox(width: 12),
+                            Text(
+                              "Linguagem",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                            Spacer(),
+                            Icon(
+                              CupertinoIcons.chevron_right,
                               color: Colors.white,
-                              fontWeight: FontWeight.w400),
+                            ),
+                          ],
                         ),
-                        Spacer(),
-                        Icon(Icons.arrow_forward_ios, color: Colors.white),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 10),
+                      ),
+                      const SizedBox(height: 10),
 
-                  TextButton(
-                    onPressed: () {
-                      // Adicione aqui o código que deve ser executado quando o botão for pressionado
-                    },
-                    child: Row(
-                      children: const [
-                        Icon(Icons.edit_square, color: Colors.white),
-                        SizedBox(width: 8),
-                        Text(
-                          "Editar Perfil",
-                          style: TextStyle(
-                              fontSize: 16,
+                      TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: const [
+                            Icon(CupertinoIcons.pencil_outline,
+                                color: Colors.white),
+                            SizedBox(width: 12),
+                            Text(
+                              "Editar Perfil",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                            Spacer(),
+                            Icon(
+                              CupertinoIcons.chevron_right,
                               color: Colors.white,
-                              fontWeight: FontWeight.w400),
+                            ),
+                          ],
                         ),
-                        Spacer(),
-                        Icon(Icons.arrow_forward_ios, color: Colors.white),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 10),
+                      ),
+                      const SizedBox(height: 10),
 
-                  TextButton(
-                    onPressed: () {
-                      // Adicione aqui o código que deve ser executado quando o botão for pressionado
-                    },
-                    child: Row(
-                      children: const [
-                        Icon(Icons.settings_outlined, color: Colors.white),
-                        SizedBox(width: 8),
-                        Text(
-                          "Configurações",
-                          style: TextStyle(
-                              fontSize: 16,
+                      TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: const [
+                            Icon(CupertinoIcons.settings, color: Colors.white),
+                            SizedBox(width: 12),
+                            Text(
+                              "Configurações",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                            Spacer(),
+                            Icon(
+                              CupertinoIcons.chevron_right,
                               color: Colors.white,
-                              fontWeight: FontWeight.w400),
+                            ),
+                          ],
                         ),
-                        Spacer(),
-                        Icon(Icons.arrow_forward_ios, color: Colors.white),
-                      ],
-                    ),
-                  ),
-                  //SizedBox(height: 10),
+                      ),
+                      //SizedBox(height: 10),
 
-                  TextButton(
-                    onPressed: () {
-                      // Adicione aqui o código que deve ser executado quando o botão for pressionado
-                    },
-                    child: Row(
-                      children: const [
-                        Icon(Icons.info_outline, color: Colors.white),
-                        SizedBox(width: 8),
-                        Text(
-                          "Ajuda & Perguntas Frequentes",
-                          style: TextStyle(
-                              fontSize: 16,
+                      TextButton(
+                        onPressed: () {
+                          // Adicione aqui o código que deve ser executado quando o botão for pressionado
+                        },
+                        child: Row(
+                          children: const [
+                            Icon(
+                              CupertinoIcons.info_circle,
                               color: Colors.white,
-                              fontWeight: FontWeight.w400),
+                            ),
+                            SizedBox(width: 12),
+                            Text(
+                              "Ajuda & Perguntas Frequentes",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                            Spacer(),
+                            Icon(
+                              CupertinoIcons.chevron_right,
+                              color: Colors.white,
+                            ),
+                          ],
                         ),
-                        Spacer(),
-                        Icon(Icons.arrow_forward_ios, color: Colors.white),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 40),
-
-                  SizedBox(
-                      width: 331,
+                      ),
+                    ]),
+                    SizedBox(
+                      width: double.infinity,
                       child: ElevatedButton(
                         onPressed: () {
                           Navigator.pushNamed(context, '/login');
                           // Adicione aqui o código que deve ser executado quando o botão for pressionado
                         },
                         style: ButtonStyle(
-                          elevation: MaterialStateProperty.all<double>(0),
-                          padding:
-                              MaterialStateProperty.all<EdgeInsetsGeometry>(
-                                  EdgeInsets.symmetric(
-                                      horizontal: 16.0, vertical: 8.0)),
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              Colors.transparent),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                          elevation: MaterialStateProperty.all(0),
+                          padding: MaterialStateProperty.all(
+                            const EdgeInsets.all(12.0),
+                          ),
+                          backgroundColor: MaterialStateProperty.all(
+                            Colors.transparent,
+                          ),
+                          shape: MaterialStateProperty.all(
                             RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(8.0),
-                              side: BorderSide(
-                                  color: Color(0xFF3F4250), width: 1),
+                              side: const BorderSide(
+                                width: 2,
+                                color: Color.fromARGB(255, 80, 63, 63),
+                              ),
                             ),
                           ),
                         ),
                         child: const Text(
                           "Terminar Sessão",
                           style: TextStyle(
-                              fontSize: 16,
-                              color: Color(0xFFFD8F8F),
-                              fontWeight: FontWeight.w400),
+                            fontSize: 16,
+                            color: Colors.redAccent,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
-                      )),
-                ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            )
+            ),
           ],
         ),
       ),
