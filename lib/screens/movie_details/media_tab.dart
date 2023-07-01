e868f07afb7995d7ea4f84bb512c36b94d814076
Eliana Neto <eliananeto29@gmail.com>
2023-07-01 02:08:29 +0100

    refactor: media tab refactoring

--- lib/screens/movie_details/media_tab.dart
+++ lib/screens/movie_details/media_tab.dart
@@ -3,6 +3,7 @@ import 'package:filmoteca_app/services/get_movies.dart';
 import 'package:filmoteca_app/utils/app_colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MediaTab extends StatefulWidget {
   final int movieId;
@@ -14,18 +15,29 @@ class MediaTab extends StatefulWidget {
 }
 
 class _MediaTab extends State<MediaTab> {
-  late Future<List<MovieTrailerModel>> _movieVideoFuture;
+  late Future<List<List<MovieTrailerModel>>> _movieMidia;
 
   @override
   void initState() {
     super.initState();
-    _movieVideoFuture = fetchVideosByMovieId(movieId: widget.movieId);
+    //_movieVideosFuture = fetchVideosByMovieId(movieId: widget.movieId);
+    //_movieImagesFuture = fetchImagesByMovieId(movieId: widget.movieId);
+
+    _movieMidia = fetchMediaByMovieId(movieId: widget.movieId);
+  }
+
+  void _launchURL(Uri url) async {
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Não foi possível abrir o URL: $url';
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<MovieTrailerModel>>(
-      future: _movieVideoFuture,
+    return FutureBuilder<List<List<MovieTrailerModel>>>(
+      future: _movieMidia,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
@@ -38,46 +50,77 @@ class _MediaTab extends State<MediaTab> {
             ),
           );
         } else {
-          List<MovieTrailerModel> moviesVideos = snapshot.data!;
+          List<List<MovieTrailerModel>> moviesVideos = snapshot.data!;
+          List<MovieTrailerModel> moviesImgList = moviesVideos[0];
+          List<MovieTrailerModel> moviesVidList = moviesVideos[1];
+          print('imagens: ${moviesImgList.length}');
+          print('videos: ${moviesVidList.length}');
+
           return SizedBox(
             height: 400,
-            child: GridView.builder(
-              itemCount: moviesVideos.length,
+            child: ListView.builder(
+              itemCount: moviesImgList.length < moviesVidList.length
+                  ? moviesImgList.length
+                  : moviesVidList.length,
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              padding: const EdgeInsets.symmetric(horizontal: 0),
               itemBuilder: (BuildContext context, index) {
-                return Row(
-                  children: [
-                    Container(
-                      width: 115,
-                      height: 70,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 14, vertical: 10),
-                      decoration: BoxDecoration(
-                        color: AppColors.darkSecondaryColor,
-                        borderRadius: BorderRadius.circular(10),
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 15),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          final videoUrl = Uri(
+                              scheme: 'https',
+                              host: 'youtube.com',
+                              path: '/watch',
+                              queryParameters: {
+                                'v': moviesVidList[index].videoKey
+                              });
+
+                          print(videoUrl);
+                          _launchURL(videoUrl);
+                        },
+                        child: Container(
+                          width: 115,
+                          height: 70,
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: AppColors.darkSecondaryColor,
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: NetworkImage(moviesImgList[index].imgUrl),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          child: const Icon(
+                            CupertinoIcons.play_fill,
+                            size: 40,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      moviesVideos[index].title,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w400),
-                    ),
-                  ],
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: Text(
+                          moviesVidList[index].title,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               },
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                childAspectRatio: MediaQuery.of(context).size.width /
-                    (50 * MediaQuery.of(context).devicePixelRatio),
-              ),
             ),
           );
         }
